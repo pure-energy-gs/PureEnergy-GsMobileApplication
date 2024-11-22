df86c2d064d0f3c43c28d6b716cf137d97ccfaa0
Keven Ike <kevenikepsilva@gmail.com>
2024-11-22 04:29:02 -0300

    projeto

--- PureEnergy/app/src/main/java/br/com/fiap/pureenergy/HomeFragment.kt
+++ PureEnergy/app/src/main/java/br/com/fiap/pureenergy/HomeFragment.kt
@@ -29,6 +29,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentHomeBinding.bind(view)
 
+
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
 
